f9b64867cccde3bc95a5cbeb8abf25fb3dec582d
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-12-07 08:56:33 -0700

    changed import

--- src/texture/pack/Texture.java
+++ src/texture/pack/Texture.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package com.watabou.glwrap;
+package texture.pack;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
